69d92f42c2a3a85dd4ba7bb4007fbcc4174e909b
AlaaElsayed14 <alaaelsayed1433@gmail.com>
2022-06-22 01:21:41 +0200

    calculate the salary

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var salary = parseInt(prompt("enter your salary"));
+
+if(salary>5000 && salary<10000){
+    console.log("rich");
+}else if(salary>10000){
+    console.log("wealthy")
+}else{
+    console.log("mashia be elsatr")
+}
